a3d2cc03aefb9ca7b790e61a350bbf13a79875ac
Zum Nebel Tech <zumnebeltech@proton.me>
2025-06-15 21:08:51 -0300

    Added COPRs and packages
    Add lines to enable COPRs and install packages for Hyprland etc.

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,13 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf -y copr enable pgdev/ghostty
+dnf -y copr enable peterwu/iosevka
+dnf -y copr enable solopasha/hyprland
+dnf -y copr enable tofik/nwg-shell
+dnf -y copr enable atim/starship
+dnf -y copr enable peterwu/iosevka
+dnf -y install fish atuin hyprland hyprland-plugin-hyprbars stow hyprlock hypridle hyprpaper hyprlock hyprshot hyprdim hyprsunset cliphist waybar waypaper blueman network-manager-applet xwaylandvideobridge udiskie mpv feh zoxide jq qt6ct qt5ct qt6-qtwayland qt5-qtwayland copyq lsd ImageMagick GraphicsMagick swww wlogout wl-paste wl-clipboard wl-copy nwg-clipman nwg-shell-config nwg-drawer python-psutil fd brightnessctl btrbk micro restic bat cosmic-term starship swaync cargo satty vips uwsm breeze-gtk plasma-breeze rsms-inter{,-vf}-fonts jetbrains-mono-fonts-all iosevka-etoile-fonts meson cmake scdoc dmenu python3-pyxdg python3-dbus gtk3-devel gtk4-devel gobject-introspection-devel gtk-layer-shell-devel kdeconnectd kde-connect-nautilus
 
 # Use a COPR Example:
 #
